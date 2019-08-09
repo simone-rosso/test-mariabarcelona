3ab29288d3e381ea6d985372e3ee5475b7eecdc3
simonredau <simon.red.au@gmail.com>
2019-08-09 17:50:08 +0200

    fixed form, api call

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
 import TextField from '@material-ui/core/TextField';
 import './App.css';
-import { Typography, Button } from '@material-ui/core';
+import { Button, MenuItem, Select, OutlinedInput } from '@material-ui/core';
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
@@ -13,11 +12,12 @@ import FormLabel from '@material-ui/core/FormLabel';
 const defaultInfos = {
   name: "",
   surname: "",
-  day: undefined,
-  documentType: undefined,
-  documentNumber: undefined,
+  gender: "",
+  id: undefined,
+  document_type: "none",
+  identification: undefined,
   email: "",
-  repeatEmail: ""
+  password: ""
 }
 
 class App extends Component {
@@ -25,13 +25,12 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      infos: defaultInfos,
-      gender: "a"
+      infos: defaultInfos
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.sendInfo = this.sendInfo.bind(this);
-    this.reset = this.reset.bind(this);
+    this.apiCall = this.apiCall.bind(this);
     this.handleRadioCheck = this.handleRadioCheck.bind(this);
   }
 
@@ -48,12 +47,30 @@ class App extends Component {
   }
 
   sendInfo() {
-    console.log(this.state)
+    fetch("https://my-json-server.typicode.com/volkz/technical-form/users", {
+      method: 'POST',
+      body: this.state.infos,
+      type: JSON
+    }).then((result)=> console.log(result));
   }
 
-  reset() {
-    console.log(defaultInfos)
-    this.setState({ infos: defaultInfos })
+  apiCall() {
+    fetch("https://my-json-server.typicode.com/volkz/technical-form/users")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({infos:result[0]})
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
   }
 
   render() {
@@ -90,18 +107,21 @@ class App extends Component {
                 <div className="birthday-container">
                   <TextField
                     id="outlined-date-day"
+                    className="date-fields"
                     type="number"
                     margin="normal"
                     variant="outlined"
                   />
                   <TextField
                     id="outlined-date-month"
+                    className="date-fields"
                     type="number"
                     margin="normal"
                     variant="outlined"
                   />
                   <TextField
                     id="outlined-date-year"
+                    className="date-fields"
                     type="number"
                     margin="normal"
                     variant="outlined"
@@ -109,81 +129,86 @@ class App extends Component {
                 </div>
               </div>
 
-
-
-
               <div className="row col-sm-6 form-document">
                 <label>Documento</label>
                 <div className="birthday-container">
-                  <div className="col-sm-5 document-type">
-                    <TextField
-                      id="outlined-document-type"
-                      onChange={(e) => this.handleChange(e, "document-type")}
-                      margin="normal"
-                      variant="outlined"
-                    />
-                  </div>
+
+                  <FormControl variant="outlined" >
+                    <Select
+                      value={this.state.infos.document_type}
+                      onChange={(e) => this.handleChange(e, "document_type")}
+                      input={<OutlinedInput />}>
+                      <MenuItem value="none">
+                      </MenuItem>
+                      <MenuItem value="DNI">DNI</MenuItem>
+                      <MenuItem value="NIE">NIE</MenuItem>
+                      <MenuItem value="PASSPORT">PASSPORT</MenuItem>
+                    </Select>
+                  </FormControl>
+
                   <div className="col-sm-7 document-number">
                     <TextField
                       id="outlined-document-number"
+                      value={this.state.infos.identification}
                       onChange={(e) => this.handleChange(e, "document-number")}
                       margin="normal"
                       variant="outlined"
                     />
                   </div>
                 </div>
-            </div>
+              </div>
 
-            <div className="col-sm-12 form-gender">
-              <FormControl component="fieldset">
-                <FormLabel component="legend">Genero</FormLabel>
-                <RadioGroup aria-label="position" name="position" value={this.state.gender} onChange={this.handleRadioCheck} row>
-                  <FormControlLabel
-                    value="a"
-                    control={<Radio color="primary" />}
-                    label="Mujer"
-                    labelPlacement="end"
-                  />
-                  <FormControlLabel
-                    value="b"
-                    control={<Radio color="primary" />}
-                    label="Hombre"
-                    labelPlacement="end"
-                  />
-                  <FormControlLabel
-                    value="c"
-                    control={<Radio color="primary" />}
-                    label="Prefiero no contestar"
-                    labelPlacement="end"
-                  />
-                </RadioGroup>
-              </FormControl>
-            </div>
-            <div className="col-sm-6 form-email">
-              <TextField
-                id="outlined-email"
-                label="Email"
-                value={this.state.infos.email}
-                onChange={(e) => this.handleChange(e, "email")}
-                margin="normal"
-                variant="outlined"
-              />
-            </div>
-            <div className="col-sm-6 form-repeat-email">
-              <TextField
-                id="outlined-repeat-email"
-                label="Repeat e-mail"
-                value={this.state.infos.repeatEmail}
-                onChange={(e) => this.handleChange(e, "repeatEmail")}
-                margin="normal"
-                variant="outlined"
-              />
+              <div className="col-sm-12 form-gender">
+                <FormControl component="fieldset">
+                  <FormLabel component="legend">Genero</FormLabel>
+                  <RadioGroup aria-label="position" name="position" value={this.state.gender} onChange={this.handleRadioCheck} row>
+                    <FormControlLabel
+                      value="a"
+                      control={<Radio color="primary" />}
+                      label="Mujer"
+                      labelPlacement="end"
+                    />
+                    <FormControlLabel
+                      value="b"
+                      control={<Radio color="primary" />}
+                      label="Hombre"
+                      labelPlacement="end"
+                    />
+                    <FormControlLabel
+                      value="c"
+                      control={<Radio color="primary" />}
+                      label="Prefiero no contestar"
+                      labelPlacement="end"
+                    />
+                  </RadioGroup>
+                </FormControl>
+              </div>
+              <div className="col-sm-6 form-email">
+                <TextField
+                  id="outlined-email"
+                  label="Email"
+                  value={this.state.infos.email}
+                  onChange={(e) => this.handleChange(e, "email")}
+                  margin="normal"
+                  variant="outlined"
+                />
+              </div>
+              <div className="col-sm-6 form-repeat-email">
+                <TextField
+                  id="outlined-repeat-email"
+                  label="Repeat e-mail"
+                  value={this.state.infos.email}
+                  onChange={(e) => this.handleChange(e, "email")}
+                  margin="normal"
+                  variant="outlined"
+                />
+              </div>
             </div>
           </div>
-          </div>
 
 
-        {/*<Button variant="contained" color="primary" onClick={this.sendInfo}> Send</Button> */}
+          <Button variant="contained" color="primary" onClick={this.sendInfo}> Send</Button>
+          <Button variant="contained" color="primary" onClick={this.apiCall}>Call</Button>
         </Container>
       </div >
     );
